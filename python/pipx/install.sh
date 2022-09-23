9f4251fcfa55fd9e86d1efa09145a8126ba682d1
max533 <25530573+max533@users.noreply.github.com>
2022-09-23 13:39:28 +0000

    fix(python-pipx): fix pyenv initialized failed when installing pipx

--- python/pipx/install.sh
+++ python/pipx/install.sh
@@ -1,3 +1,8 @@
+# Initialize Pyenv (Use python which pyenv install to create virtualenv environment)
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
+
 # Declare pipx variable
 export PIPX_HOME=~/.local/pipx
 export PIPX_BIN_DIR=~/.local/bin
@@ -10,14 +15,12 @@ echo "PIPX_HOME=$PIPX_BIN_DIR" >> ~/.bashrc
 echo "PIPX_BIN_DIR=$PIPX_BIN_DIR" >> ~/.bashrc
 echo "PIPX_SETUP_FOLDER=$PIPX_SETUP_FOLDER" >> ~/.bashrc
 
-
-
 # Create python virtual environment
 mkdir -p $PIPX_SETUP_FOLDER
 python -m venv $PIPX_SETUP_FOLDER
 
-# Install pipx 
-$PIPX_SETUP_FOLDER/bin/python -m pip install pipx 
+# Install pipx
+$PIPX_SETUP_FOLDER/bin/python -m pip install pipx
 $PIPX_SETUP_FOLDER/bin/python -m pipx ensurepath
 
 # Make pipx soft link
