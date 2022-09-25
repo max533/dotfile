cb8ae6b3992dac3bc11186c08b5503e6274b0f24
max533 <25530573+max533@users.noreply.github.com>
2022-09-25 16:59:35 +0800

    feat(shell-theme): add python venv segment in customized oh my posh theme

--- shell/theme/install.sh
+++ shell/theme/install.sh
@@ -7,12 +7,17 @@ sudo chmod +x /usr/local/bin/oh-my-posh
 
 mkdir ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
-chmod u+rw ~/.poshthemes/*.omp.*
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes/official_themes
+chmod u+rw ~/.poshthemes/official_themes/*.omp.*
 rm ~/.poshthemes/themes.zip
+
+mkdir -p ~/.poshthemes/customized_themes/
+cp ~/dotfile/shell/themes/montys_customized.omp.json ~/.poshthemes/customized_themes/montys_customized.omp.json
+chmod u+rw ~/.poshthemes/customized_themes/*.omp.*
 
 # Initialize with shell
 echo "" >> ~/.bashrc
 echo "# Set shell prompt theme with Oh My Push" >> ~/.bashrc
-echo "eval \"\$(oh-my-posh --init --shell bash --config ~/.poshthemes/montys.omp.json)\"" >> ~/.bashrc
+echo "eval \"\$(oh-my-posh --init --shell bash --config ~/.poshthemes/customized_themes/montys_customized.omp.json)\"" >> ~/.bashrc
 source ~/.bashrc
+
