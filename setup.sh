7e4c8304f58ba9639c1e4e8d19e6ea703603a6dc
max533 <25530573+max533@users.noreply.github.com>
2022-09-26 08:08:37 +0000

    fix: cancel vscode installation and add install execution progress

--- setup.sh
+++ setup.sh
@@ -1,21 +1,47 @@
+echo ''
 echo '================================'
 echo 'Ready to setup development environment. Wait for moment.'
+echo '================================'
+echo ''
 
 WORKDIR="$HOME/dotfile"
+
+echo ''
+echo '================================'
+echo 'Install Bash components'
+echo '================================'
+echo ''
 
 cd $WORKDIR/shell/bash
 ./install.sh
 
+echo ''
+echo '================================'
+echo 'Install Theme components'
+echo '================================'
+echo ''
+
 cd $WORKDIR/shell/theme
 ./install.sh
 
-cd $WORKDIR/vscode
-./install.sh
+echo ''
+echo '================================'
+echo 'Install Pyenv components'
+echo '================================'
+echo ''
 
 cd $WORKDIR/python/pyenv
 ./install.sh
 
+echo '================================'
+echo 'Install Pipx components'
+echo '================================'
+
 cd $WORKDIR/python/pipx
 ./install.sh
+
+echo ''
 echo '================================'
 echo 'Development environment is ready.'
+echo '================================'
+echo ''
